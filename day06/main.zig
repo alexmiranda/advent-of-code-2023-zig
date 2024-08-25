066ecab9fe2259bfa9f9aa065a03fd1f6fb0d168
Alex Miranda <alexmirandamoraes@gmail.com>
2024-08-25 04:37:09 +0200

    day 6 - part 2

--- day06/main.zig
+++ day06/main.zig
@@ -22,8 +22,7 @@ const Race = struct {
 
 const List = std.BoundedArray(Race, 4);
 
-fn parseRaces(allocator: std.mem.Allocator, s: []const u8) !List {
-    _ = allocator;
+fn parseRaces(s: []const u8) !List {
     var it = std.mem.splitScalar(u8, s, '\n');
     var times = readLine(it.next().?);
     var distances = readLine(it.next().?);
@@ -38,13 +37,29 @@ fn parseRaces(allocator: std.mem.Allocator, s: []const u8) !List {
     return list;
 }
 
+fn parseSingleRace(allocator: std.mem.Allocator, s: []const u8) !Race {
+    var it = std.mem.splitScalar(u8, s, '\n');
+    const time = try readNum(allocator, it.next().?);
+    const distance = try readNum(allocator, it.next().?);
+    return .{ .time = time, .distance = distance };
+}
+
 fn readLine(s: []const u8) std.mem.TokenIterator(u8, .scalar) {
     const idx = std.mem.indexOfScalar(u8, s, ':').?;
     return std.mem.tokenizeScalar(u8, s[idx + 1 ..], ' ');
 }
 
+fn readNum(allocator: std.mem.Allocator, s: []const u8) !usize {
+    const idx = std.mem.indexOfScalar(u8, s, ':').?;
+    var buf = try allocator.alloc(u8, s[idx + 1 ..].len);
+    defer allocator.free(buf);
+
+    const replacements = std.mem.replace(u8, s[idx + 1 ..], " ", "", buf);
+    return try std.fmt.parseInt(usize, buf[0..(buf.len - replacements)], 10);
+}
+
 test "example - part 1" {
-    const races = try parseRaces(std.testing.allocator, example);
+    const races = try parseRaces(example);
     var result: i32 = 1;
     for (races.slice()) |race| {
         result *= race.computeNumberOfWins();
@@ -53,10 +68,20 @@ test "example - part 1" {
 }
 
 test "input - part 1" {
-    const races = try parseRaces(std.testing.allocator, input);
+    const races = try parseRaces(input);
     var result: i32 = 1;
     for (races.slice()) |race| {
         result *= race.computeNumberOfWins();
     }
     try std.testing.expectEqual(@as(i32, 393120), result);
+}
+
+test "example - part 2" {
+    const race = try parseSingleRace(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i32, 71503), race.computeNumberOfWins());
+}
+
+test "input - part 2" {
+    const race = try parseSingleRace(std.testing.allocator, input);
+    try std.testing.expectEqual(@as(i32, 36872656), race.computeNumberOfWins());
 }
