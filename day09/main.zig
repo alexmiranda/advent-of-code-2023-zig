189d3accbf8c1a8e43bad9252383753e4b297935
Alex Miranda <alexmirandamoraes@gmail.com>
2024-08-29 20:55:44 +0200

    day 9 - part 2: bit of fun with zig metaprogramming

--- day09/main.zig
+++ day09/main.zig
@@ -3,113 +3,103 @@ const print = std.debug.print;
 const example = @embedFile("example.txt");
 const input = @embedFile("input.txt");
 
-const StepsIterator = struct {
-    allocator: std.mem.Allocator,
-    lineIterator: std.mem.TokenIterator(u8, .scalar),
-
-    fn next(self: *@This()) !?[]i32 {
-        if (self.lineIterator.next()) |line| {
-            const size = std.mem.count(u8, line, " ") + 1;
-            const step = try self.allocator.alloc(i32, size);
-
-            // parse all the numbers in the next step
-            var it = std.mem.splitScalar(u8, line, ' ');
-            var slide: usize = 0;
-            while (it.next()) |s| : (slide += 1) {
-                step[slide] = try std.fmt.parseInt(i32, s, 10);
-            }
-            return step;
-        }
-        return null;
-    }
+const Part = enum {
+    part_1,
+    part_2,
 };
 
-fn solve(s: []const u8, predictFn: *const fn (std.mem.Allocator, []i32) i32, comptime max_items_per_step: usize) !i32 {
-    // by knowing the maximum number of items per line, we can calculate the amount of bytes that we need
-    // by calculating the triangular number and multiplying it by the number of bytes for each item
-    const max_capacity = @sizeOf(i32) / @sizeOf(u8) * (max_items_per_step * (max_items_per_step + 1) / 2);
-    var buf: [max_capacity]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buf);
-    var childAllocator = fba.allocator();
+fn Solver(comptime T: type, comptime max_items_per_step: usize) type {
+    const StepsIterator = struct {
+        allocator: std.mem.Allocator,
+        lineIterator: std.mem.TokenIterator(u8, .scalar),
 
-    var it = StepsIterator{
-        .allocator = childAllocator,
-        .lineIterator = std.mem.tokenizeScalar(u8, s, '\n'),
+        fn next(self: *@This()) !?[]T {
+            if (self.lineIterator.next()) |line| {
+                const size = std.mem.count(u8, line, " ") + 1;
+                const step = try self.allocator.alloc(T, size);
+
+                // parse all the numbers in the next step
+                var it = std.mem.splitScalar(u8, line, ' ');
+                var slide: usize = 0;
+                while (it.next()) |s| : (slide += 1) {
+                    step[slide] = try std.fmt.parseInt(T, s, 10);
+                }
+                return step;
+            }
+            return null;
+        }
     };
 
-    var sum: i32 = 0;
-    while (try it.next()) |step| {
-        defer childAllocator.free(step);
-        sum += predictFn(childAllocator, step);
-    }
+    return struct {
+        fn solve(s: []const u8, comptime part: Part) !T {
+            // by knowing the maximum number of items per line, we can calculate the amount of bytes that we need
+            // by calculating the triangular number and multiplying it by the number of bytes for each item
+            const max_capacity = @sizeOf(T) / @sizeOf(u8) * (max_items_per_step * (max_items_per_step + 1) / 2);
+            var buf: [max_capacity]u8 = undefined;
+            var fba = std.heap.FixedBufferAllocator.init(&buf);
+            var allocator = fba.allocator();
+            // print("max_capacity: {d}\n", .{max_capacity});
 
-    return sum;
-}
+            var it = StepsIterator{
+                .allocator = allocator,
+                .lineIterator = std.mem.tokenizeScalar(u8, s, '\n'),
+            };
 
-fn predictNext(allocator: std.mem.Allocator, step: []i32) i32 {
-    if (step.len == 1 or std.mem.allEqual(i32, step, 0)) {
-        return 0;
-    }
+            var sum: T = 0;
+            while (try it.next()) |step| {
+                defer allocator.free(step);
+                sum += predict(allocator, step, part);
+            }
 
-    // ok because we always pass down a fixed buffer allocator with sufficient memory
-    const nextStep = allocator.alloc(i32, step.len - 1) catch unreachable;
-    defer allocator.free(nextStep);
+            return sum;
+        }
 
-    var slide: usize = 0;
-    for (step[0 .. step.len - 1], step[1..]) |a, b| {
-        nextStep[slide] = b - a;
-        slide += 1;
-    }
+        fn predict(allocator: std.mem.Allocator, step: []T, comptime part: Part) T {
+            if (step.len == 1 or std.mem.allEqual(T, step, 0)) {
+                return 0;
+            }
 
-    const predicted = predictNext(allocator, nextStep);
-    // print("predicted: {d}\n", .{predicted});
-    return step[step.len - 1] + predicted;
-}
+            // safe because we always pass down a fixed buffer allocator with sufficient memory
+            const next_step = allocator.alloc(T, step.len - 1) catch unreachable;
+            defer allocator.free(next_step);
 
-fn predictBackwards(allocator: std.mem.Allocator, step: []i32) i32 {
-    if (step.len == 1 or std.mem.allEqual(i32, step, 0)) {
-        return 0;
-    }
+            var slide: usize = 0;
+            for (step[0 .. step.len - 1], step[1..]) |a, b| {
+                next_step[slide] = b - a;
+                slide += 1;
+            }
 
-    // ok because we always pass down a fixed buffer allocator with sufficient memory
-    const nextStep = allocator.alloc(i32, step.len - 1) catch unreachable;
-    defer allocator.free(nextStep);
-
-    var slide: usize = 0;
-    for (step[0 .. step.len - 1], step[1..]) |a, b| {
-        nextStep[slide] = b - a;
-        slide += 1;
-    }
-
-    const predicted = predictBackwards(allocator, nextStep);
-    // print("predicted: {d}\n", .{predicted});
-    return step[0] - predicted;
-}
-
-fn printStep(step: []i32) void {
-    print("{d}", .{step[0]});
-    for (step[1..]) |value| {
-        print(" {d}", .{value});
-    }
-    print("\n", .{});
+            const predicted = predict(allocator, next_step, part);
+            // print("predicted: {d}\n", .{predicted});
+            if (part == .part_1) {
+                return step[step.len - 1] + predicted;
+            } else {
+                return step[0] - predicted;
+            }
+        }
+    };
 }
 
 test "example - part 1" {
-    const solution = try solve(example, predictNext, 6);
-    try std.testing.expectEqual(@as(i32, 114), solution);
+    const ExampleSolver = Solver(u8, 6);
+    const solution = try ExampleSolver.solve(example, .part_1);
+    try std.testing.expectEqual(@as(u8, 114), solution);
 }
 
 test "input - part 1" {
-    const solution = try solve(input, predictNext, 21);
+    const InputSolver = Solver(i32, 21);
+    const solution = try InputSolver.solve(input, .part_1);
     try std.testing.expectEqual(@as(i32, 1696140818), solution);
 }
 
 test "example - part 2" {
-    const solution = try solve(example, predictBackwards, 6);
-    try std.testing.expectEqual(@as(i32, 2), solution);
+    const ExampleSolver = Solver(i8, 6);
+    const solution = try ExampleSolver.solve(example, .part_2);
+    try std.testing.expectEqual(@as(i8, 2), solution);
 }
 
 test "input - part 2" {
-    const solution = try solve(input, predictBackwards, 21);
+    const InputSolver = Solver(i32, 21);
+    const solution = try InputSolver.solve(input, .part_2);
     try std.testing.expectEqual(@as(i32, 1152), solution);
 }
