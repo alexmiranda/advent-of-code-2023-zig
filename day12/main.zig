5c15de7b602a2d56a1db36e08c3ea8b401080dd2
Alex Miranda <alexmirandamoraes@gmail.com>
2024-09-07 20:10:57 +0200

    day 12 - part 2

--- day12/main.zig
+++ day12/main.zig
@@ -13,6 +13,28 @@ const Spring = enum(u2) {
     unknown = 0,
 };
 
+const CacheKey = struct {
+    springs_left: usize,
+    counts_left: usize,
+};
+
+const CacheKeyContext = struct {
+    pub fn hash(ctx: @This(), key: CacheKey) u64 {
+        _ = ctx;
+        var h = std.hash.Fnv1a_64.init();
+        h.update(&std.mem.toBytes(key.springs_left));
+        h.update(&std.mem.toBytes(key.counts_left));
+        return h.final();
+    }
+
+    pub fn eql(ctx: @This(), lhs: CacheKey, rhs: CacheKey) bool {
+        _ = ctx;
+        return lhs.springs_left == rhs.springs_left and lhs.counts_left == rhs.counts_left;
+    }
+};
+
+const Cache = std.HashMap(CacheKey, u64, CacheKeyContext, std.hash_map.default_max_load_percentage);
+
 const Record = struct {
     allocator: mem.Allocator,
     springs: []Spring,
@@ -56,63 +78,121 @@ const Record = struct {
     }
 
     fn solve(self: *Record) !u64 {
-        // print("=== new search ===\n", .{});
-        // print("springs: ", .{});
-        // printSprings(self.springs);
-        // print("counts: {d}\n", .{self.counts});
-        var arena = heap.ArenaAllocator.init(self.allocator);
-        defer arena.deinit();
-        const allocator = arena.allocator();
-
-        const State = struct {
-            springs: []Spring,
-            pos: usize = 0,
-        };
-
-        var mutations = std.ArrayList(State).init(allocator);
-        defer mutations.deinit();
-
-        {
-            const springs = try clone(allocator, self.springs);
-            errdefer allocator.free(springs);
-            @memcpy(springs, self.springs);
-            try mutations.append(.{ .springs = springs, .pos = 0 });
-        }
-
-        var counter: u64 = 0;
-        while (mutations.items.len > 0) {
-            const state = mutations.pop();
-            defer allocator.free(state.springs);
-
-            if (mem.indexOfScalarPos(Spring, state.springs, state.pos, .unknown)) |index| {
-                {
-                    var springs = try clone(allocator, state.springs);
-                    errdefer allocator.free(springs);
-                    springs[index] = .operational;
-                    try mutations.append(.{ .springs = springs, .pos = index + 1 });
-                }
-
-                {
-                    var springs = try clone(allocator, state.springs);
-                    errdefer allocator.free(springs);
-                    springs[index] = .damaged;
-                    try mutations.append(.{ .springs = springs, .pos = index + 1 });
-                }
-            } else if (try verify(allocator, state.springs, self.counts)) {
-                // print("solution: ", .{});
-                // printSprings(state.springs);
-                counter += 1;
-            }
-        }
-
-        return counter;
+        var cache = Cache.init(self.allocator);
+        defer cache.deinit();
+        return try countArrangements(self.springs, self.counts, null, &cache);
     }
 
-    fn clone(allocator: mem.Allocator, springs: []Spring) ![]Spring {
-        const copy = try allocator.alloc(Spring, springs.len);
-        errdefer allocator.free(copy);
-        @memcpy(copy, springs);
-        return copy;
+    fn countArrangements(springs: []Spring, counts: []u8, assumed: ?Spring, cache: *Cache) !u64 {
+        // print("springs: ", .{});
+        // printSprings(springs);
+        // print("counts: {d}\n", .{counts});
+        // print("----------\n", .{});
+        const cacheKey = CacheKey{ .springs_left = springs.len, .counts_left = counts.len };
+        if (cache.get(cacheKey)) |result| {
+            return result;
+        }
+
+        // if there's nothing else to match and no damaged springs left, then we are over!
+        if (counts.len == 0) {
+            return if (mem.containsAtLeast(Spring, springs, 1, &[_]Spring{.damaged})) 0 else 1;
+        }
+
+        // no springs left...
+        if (springs.len == 0) {
+            return 0;
+        }
+
+        // sum up all the remaining groups' count to be matched
+        const to_be_matched = blk: {
+            var sum: u8 = 0;
+            for (counts) |count| {
+                sum += count;
+            }
+            break :blk sum;
+        };
+
+        // count the remaining damaged and unknown springs, aka, unmatched springs
+        const unmatched = springs.len - mem.count(Spring, springs, &[_]Spring{.operational});
+
+        // if there insufficient unmatched springs, then we cannot proceed
+        if (to_be_matched > unmatched) {
+            return 0;
+        }
+
+        const spring = assumed orelse springs[0];
+        const result: u64 = switch (spring) {
+            .operational => blk: {
+                // skip all operational springs and proceed
+                const next_non_operational = mem.indexOfAny(Spring, springs, &[_]Spring{ .damaged, .unknown }) orelse springs.len - 1;
+                break :blk try countArrangements(springs[next_non_operational..], counts, null, cache);
+            },
+            .damaged => blk: {
+                // we don't have sufficient springs left to match
+                if (springs.len < counts[0]) {
+                    break :blk 0;
+                }
+
+                // there is not enough contiguous springs to match
+                if (mem.indexOfScalar(Spring, springs[0..counts[0]], .operational)) |_| {
+                    break :blk 0;
+                }
+
+                // if we have the exact amount of springs and it's the last group to match
+                if (springs.len == counts[0]) {
+                    break :blk if (counts.len == 1) 1 else 0;
+                }
+
+                // if we have more springs left to match
+                // we cannot have a damaged spring that causes the length of contiguous damaged springs to be greater
+                if (springs[counts[0]] == .damaged) {
+                    break :blk 0;
+                }
+
+                break :blk try countArrangements(springs[counts[0] + 1 ..], counts[1..], null, cache);
+            },
+            .unknown => blk: {
+                const arrangements_as_if_it_were_operational = try countArrangements(springs[1..], counts, null, cache);
+                const arrangements_as_it_it_were_damaged = try countArrangements(springs, counts, .damaged, cache);
+                break :blk arrangements_as_if_it_were_operational + arrangements_as_it_it_were_damaged;
+            },
+        };
+
+        cache.put(cacheKey, result) catch {};
+        return result;
+    }
+
+    fn unfold(self: *Record) !void {
+        const old_springs = self.springs;
+        const springs = blk: {
+            const old_len = old_springs.len;
+            const new_len = old_len * 5 + 4;
+            const unfolded = try self.allocator.alloc(Spring, new_len);
+            errdefer self.allocator.free(unfolded);
+            @memcpy(unfolded[0..old_len], old_springs);
+            var slide: usize = old_len;
+            while (slide < new_len) : (slide += old_len + 1) {
+                unfolded[slide] = .unknown;
+                @memcpy(unfolded[slide + 1 .. slide + 1 + old_len], old_springs);
+            }
+            break :blk unfolded;
+        };
+        self.springs = springs;
+        self.allocator.free(old_springs);
+
+        const old_counts = self.counts;
+        const counts = blk: {
+            const old_len = self.counts.len;
+            const new_len = old_len * 5;
+            const unfolded = try self.allocator.alloc(u8, new_len);
+            var slide: usize = 0;
+            while (slide < new_len) : (slide += old_len) {
+                @memcpy(unfolded[slide .. slide + old_len], old_counts);
+            }
+            break :blk unfolded;
+        };
+        self.counts = counts;
+        self.allocator.free(old_counts);
     }
 
     fn verify(allocator: mem.Allocator, springs: []Spring, counts: []u8) !bool {
@@ -162,7 +242,6 @@ test "example - part 1" {
 }
 
 test "input - part 1" {
-    // if (true) return error.SkipZigTest;
     var logging_allocator = heap.LoggingAllocator(.debug, .debug).init(heap.page_allocator);
     const allocator = logging_allocator.allocator();
 
@@ -175,4 +254,39 @@ test "input - part 1" {
     }
 
     try testing.expectEqual(7670, sum);
+}
+
+test "example - part 2" {
+    // if (true) return error.SkipZigTest;
+    var logging_allocator = heap.LoggingAllocator(.debug, .debug).init(testing.allocator);
+    const allocator = logging_allocator.allocator();
+
+    var sum: u64 = 0;
+    var it = mem.tokenizeScalar(u8, example, '\n');
+    while (it.next()) |line| {
+        // print("{s}\n", .{line});
+        var rec = try Record.initParse(allocator, line);
+        defer rec.deinit();
+        try rec.unfold();
+        sum += try rec.solve();
+    }
+
+    try testing.expectEqual(525152, sum);
+}
+
+test "input - part 2" {
+    // if (true) return error.SkipZigTest;
+    var logging_allocator = heap.LoggingAllocator(.debug, .debug).init(heap.page_allocator);
+    const allocator = logging_allocator.allocator();
+
+    var sum: u64 = 0;
+    var it = mem.tokenizeScalar(u8, input, '\n');
+    while (it.next()) |line| {
+        var rec = try Record.initParse(allocator, line);
+        defer rec.deinit();
+        try rec.unfold();
+        sum += try rec.solve();
+    }
+
+    try testing.expectEqual(157383940585037, sum);
 }
