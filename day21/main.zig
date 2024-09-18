6bd55f8e54dcc5dc69359be3d3d7d34e260074dc
Alex Miranda <alexmirandamoraes@gmail.com>
2024-09-18 19:33:34 +0200

    day 21 - part 2 (brute force)

--- day21/main.zig
+++ day21/main.zig
@@ -9,14 +9,13 @@ const example = @embedFile("example.txt");
 const input = @embedFile("input.txt");
 
 const Tile = enum {
-    starting_position,
     garden_plot,
     rocks,
 };
 
 const Pos = struct {
-    row: usize,
-    col: usize,
+    row: i64,
+    col: i64,
 };
 
 const Farm = struct {
@@ -50,8 +49,10 @@ const Farm = struct {
                 '.' => data[slide] = .garden_plot,
                 '#' => data[slide] = .rocks,
                 'S' => {
-                    data[slide] = .starting_position;
-                    start_pos = .{ .row = slide / size, .col = slide % size };
+                    data[slide] = .garden_plot;
+                    const row: i64 = @intCast(slide / size);
+                    const col: i64 = @intCast(slide % size);
+                    start_pos = .{ .row = row, .col = col };
                 },
                 '\n' => {
                     tiles[slide / size - 1] = data[slide - size .. slide];
@@ -116,26 +117,90 @@ const Farm = struct {
         return visited.count();
     }
 
+    fn reachablePlotsInfinite(self: *Farm, turns: u32) !u32 {
+        var arena = heap.ArenaAllocator.init(self.ally);
+        defer arena.deinit();
+        const ally = arena.allocator();
+
+        var visited = std.AutoHashMap(Pos, void).init(ally);
+        defer visited.deinit();
+
+        const Q = std.TailQueue(Pos);
+        var queue = Q{};
+        var node = try ally.create(Q.Node);
+        // this could cause a double free
+        // errdefer ally.destroy(node);
+        node.data = self.start_pos;
+
+        var buf: [4]Pos = undefined;
+        queue.append(node);
+
+        // ensure we clean up the queue at the end
+        defer while (queue.pop()) |item| ally.destroy(item);
+
+        for (0..turns) |_| {
+            visited.clearRetainingCapacity();
+            try visited.put(self.start_pos, {});
+            for (0..queue.len) |_| {
+                node = queue.popFirst().?;
+                defer ally.destroy(node);
+
+                const next_positions = self.neighboursInfinite(node.data, &buf);
+                for (next_positions) |pos| {
+                    const gop = try visited.getOrPut(pos);
+                    if (!gop.found_existing) {
+                        const new_node = try ally.create(Q.Node);
+                        errdefer ally.destroy(new_node);
+                        new_node.data = pos;
+                        queue.append(new_node);
+                    }
+                }
+            }
+        }
+
+        // self.dumpVisited(&visited);
+        return visited.count();
+    }
+
     fn neighbours(self: *Farm, pos: Pos, buf: []Pos) []Pos {
-        // print("neighbours of {any}: ", .{pos});
         var slide: usize = 0;
-        if (pos.row > 0 and self.tiles[pos.row - 1][pos.col] == .garden_plot) {
-            buf[slide] = .{ .row = pos.row - 1, .col = pos.col };
+        const row: u16 = @intCast(pos.row);
+        const col: u16 = @intCast(pos.col);
+        if (row > 0 and self.tiles[row - 1][col] == .garden_plot) {
+            buf[slide] = .{ .row = row - 1, .col = col };
             slide += 1;
         }
-        if (pos.col < self.tiles[pos.row].len - 1 and self.tiles[pos.row][pos.col + 1] == .garden_plot) {
-            buf[slide] = .{ .row = pos.row, .col = pos.col + 1 };
+        if (col < self.tiles[row].len - 1 and self.tiles[row][col + 1] == .garden_plot) {
+            buf[slide] = .{ .row = row, .col = col + 1 };
             slide += 1;
         }
-        if (pos.row < self.tiles.len - 1 and self.tiles[pos.row + 1][pos.col] == .garden_plot) {
-            buf[slide] = .{ .row = pos.row + 1, .col = pos.col };
+        if (row < self.tiles.len - 1 and self.tiles[row + 1][col] == .garden_plot) {
+            buf[slide] = .{ .row = row + 1, .col = col };
             slide += 1;
         }
-        if (pos.col > 0 and self.tiles[pos.row][pos.col - 1] == .garden_plot) {
-            buf[slide] = .{ .row = pos.row, .col = pos.col - 1 };
+        if (col > 0 and self.tiles[row][col - 1] == .garden_plot) {
+            buf[slide] = .{ .row = row, .col = col - 1 };
             slide += 1;
         }
-        // print("{any} (total={d})\n", .{ buf[0..slide], slide });
+        return buf[0..slide];
+    }
+
+    fn neighboursInfinite(self: *Farm, pos: Pos, buf: []Pos) []Pos {
+        const vec_curr_pos: @Vector(8, i64) = [_]i64{ pos.row, pos.col } ** 4;
+        const vec_delta: @Vector(8, i64) = [_]i64{ -1, 0, 0, 1, 1, 0, 0, -1 };
+        const positions = @as([8]i64, vec_curr_pos + vec_delta);
+
+        var slide: usize = 0;
+        var pairs = mem.window(i64, &positions, 2, 2);
+        while (pairs.next()) |pair| {
+            const size: i64 = @intCast(self.tiles.len);
+            const actual_row: u16 = @intCast(@mod(pair[0], size));
+            const actual_col: u16 = @intCast(@mod(pair[1], size));
+            if (self.tiles[actual_row][actual_col] == .garden_plot) {
+                buf[slide] = .{ .row = pair[0], .col = pair[1] };
+                slide += 1;
+            }
+        }
         return buf[0..slide];
     }
 
@@ -180,4 +245,46 @@ test "input - part 1" {
     defer farm.deinit();
     const reachable = try farm.reachablePlots(64);
     try expectEqual(3737, reachable);
+}
+
+test "example - part 2" {
+    var farm = try Farm.initParse(testing.allocator, example);
+    defer farm.deinit();
+
+    {
+        const reachable = try farm.reachablePlotsInfinite(6);
+        try expectEqual(16, reachable);
+    }
+
+    {
+        const reachable = try farm.reachablePlotsInfinite(10);
+        try expectEqual(50, reachable);
+    }
+
+    {
+        const reachable = try farm.reachablePlotsInfinite(50);
+        try expectEqual(1594, reachable);
+    }
+
+    {
+        const reachable = try farm.reachablePlotsInfinite(100);
+        try expectEqual(6536, reachable);
+    }
+
+    // the below examples are too slow to complete
+
+    // {
+    //     const reachable = try farm.reachablePlotsInfinite(500);
+    //     try expectEqual(167004, reachable);
+    // }
+
+    // {
+    //     const reachable = try farm.reachablePlotsInfinite(1000);
+    //     try expectEqual(668697, reachable);
+    // }
+
+    // {
+    //     const reachable = try farm.reachablePlotsInfinite(5000);
+    //     try expectEqual(16733044, reachable);
+    // }
 }
